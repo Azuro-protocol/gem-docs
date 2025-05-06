1f397c2e19730a00d81b0fd3f1f8b0428cd9c200
CleanBread <gleb4ist@gmail.com>
2025-05-06 19:13:26 +0900

    v3 notification

--- components/Callout/index.tsx
+++ components/Callout/index.tsx
@@ -4,20 +4,21 @@ import cx from 'clsx'
 
 type Props = {
   icon?: string
+  className?: string
   type: 'info' | 'warning' | 'alert' | 'feature' | 'advice' | 'reference'
 }
 
 export function Callout(props: React.PropsWithChildren<Props>) {
-  let { children, icon, type } = props
+  let { className, children, icon, type } = props
 
-  const rootClassName = cx('flex items-start mt-4 p-4 border rounded-md shadow-sm', {
+  const rootClassName = cx('flex items-start mt-4 first:mt-0 p-4 border rounded-md shadow-sm', {
     'bg-blue-50 border-blue-300 text-blue-900 dark:bg-blue-900/40 dark:border-blue-800 dark:text-blue-200': type === 'info',
     'bg-amber-50 border-amber-300 text-amber-900 dark:bg-amber-900/40 dark:border-amber-800 dark:text-orange-300': type === 'warning',
     'bg-red-50 border-red-300 text-red-900 dark:bg-red-900/40 dark:border-red-800 dark:text-red-200': type === 'alert',
     'bg-sky-50 border-sky-300 text-sky-900 dark:bg-sky-900/40 dark:border-sky-800 dark:text-sky-200': type === 'feature',
     'bg-yellow-50 border-yellow-300 text-yellow-900 dark:bg-yellow-900/40 dark:border-yellow-800 dark:text-yellow-300': type === 'advice',
     'italic': type === 'reference',
-  })
+  }, className)
 
   icon = icon || ({
     'info': 'ℹ️',
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,8 +2,16 @@ import type { ReactElement } from 'react'
 import type { AppProps } from 'next/app'
 
 import '../style.css'
+import { Callout } from 'components'
 
 
 export default function Nextra({ Component, pageProps }: AppProps): ReactElement {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Callout className="justify-center" type="warning">
+        <b>Important! We’ve moved to V3!</b> This documentation is for V2 only and will be updated in May.
+      </Callout>
+      <Component {...pageProps} />
+    </>
+  )
 }
